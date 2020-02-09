4f8d130ccc5048d7bc81a958418691096d65d5b3
Manish Kumar Lath <manishlath3445@gmail.com>
2020-02-09 18:49:03 +0530

    Requirement 1.4 | Add | Menu displayed from which user can choose an action

--- src/main/java/com/twu/biblioteca/BibliotecaApp.java
+++ src/main/java/com/twu/biblioteca/BibliotecaApp.java
@@ -1,5 +1,8 @@
 package com.twu.biblioteca;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -25,5 +28,17 @@ public class BibliotecaApp {
         }
 
         return String.valueOf(listOfBooks);
+    }
+
+    public void showMenu() throws IOException {
+        System.out.println("Select an option:\n" +
+                "1. List of books\n");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int option = Integer.parseInt(reader.readLine());
+        if (option == 1) {
+            System.out.println(getBookList());
+        } else {
+            System.out.println("Enter valid option!");
+        }
     }
 }
--- src/test/java/com/twu/biblioteca/BibliotecaAppTest.java
+++ src/test/java/com/twu/biblioteca/BibliotecaAppTest.java
@@ -1,17 +1,31 @@
 package com.twu.biblioteca;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
 
 import static org.junit.Assert.assertEquals;
 
 public class BibliotecaAppTest {
 
     BibliotecaApp biblioteca;
+    private final ByteArrayOutputStream output = new ByteArrayOutputStream();
+    private final PrintStream originaloutput = System.out;
 
     @Before
     public void setup() {
         biblioteca = new BibliotecaApp();
+        System.setOut(new PrintStream(output));
+    }
+
+    @After
+    public void reset() {
+        System.setOut(originaloutput);
     }
 
     @Test
@@ -30,5 +44,18 @@ public class BibliotecaAppTest {
         assertEquals(bookList, biblioteca.getBookList());
     }
 
+    @Test
+    public void testShouldDisplayListOfBooksIfUserChoosesItInMenu() throws IOException {
+        System.setIn(new ByteArrayInputStream("1".getBytes()));
 
+        String menuOut = "Select an option:\n" +
+                          "1. List of books\n\n";
+        String bookListString = "Harry Potter|JK Rowling|2000\n" +
+                "Da Vinci Code|Dan Brown|2003\n" +
+                "Brida|Paulo Coelho|1990\n";
+        String expectedOutput = menuOut + bookListString + "\n";
+        biblioteca.showMenu();
+
+        assertEquals(expectedOutput, output.toString());
+    }
 }
