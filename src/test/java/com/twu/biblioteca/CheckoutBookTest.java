bf82ac0e080bf33f5a751b26c848bdc142d5f91f
Manish Kumar Lath <manishlath3445@gmail.com>
2020-02-12 11:01:20 +0530

    Removed dependency of CheckoutBookTest on GetListOfBooks class
    CheckoutBookTest uses book.checkout to set the checkedOut flag to true
    
    It is verified by using the book.isCheckedOut method

--- src/test/java/com/twu/biblioteca/CheckoutBookTest.java
+++ src/test/java/com/twu/biblioteca/CheckoutBookTest.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 public class CheckoutBookTest {
@@ -20,16 +20,15 @@ public class CheckoutBookTest {
         Book bookTwo = new Book("Book2", "Author2", 2020);
         Book bookThree = new Book("Book3", "Author3", 2020);
         Collection<Book> books = new ArrayList<>(Arrays.asList(bookOne, bookTwo, bookThree));
-        GetListOfBooks getListOfBooks = new GetListOfBooks(console);
         when(console.input()).thenReturn("Book1");
         CheckoutBook checkoutBook = new CheckoutBook(console);
 
         checkoutBook.execute(books);
-        getListOfBooks.execute(books);
 
+        assertTrue(bookOne.isCheckedOut());
+        assertFalse(bookTwo.isCheckedOut());
+        assertFalse(bookThree.isCheckedOut());
 
-        verify(console, times(1)).output("Book2|Author2|2020\n" +
-                "Book3|Author3|2020\n");
     }
 
     @Test
@@ -38,12 +37,10 @@ public class CheckoutBookTest {
         Book bookOne = new Book("Book1", "Author1", 2020);
         Collection<Book> books = new ArrayList<>(Collections.singletonList(bookOne));
         CheckoutBook checkoutBook = new CheckoutBook(console);
-        GetListOfBooks getListOfBooks = new GetListOfBooks(console);
         when(console.input()).thenReturn("Book2");
 
         checkoutBook.execute(books);
-        getListOfBooks.execute(books);
 
-        verify(console, times(1)).output("Book1|Author1|2020\n");
+        assertFalse(bookOne.isCheckedOut());
     }
 }
