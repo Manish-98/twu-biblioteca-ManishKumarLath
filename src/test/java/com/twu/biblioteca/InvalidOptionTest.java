3e457da2c510e8bd81ff64fa16fe272e2bbec722
Manish Kumar Lath <manishlath3445@gmail.com>
2020-02-12 11:08:38 +0530

    Refactor | Migrated tests for invalid option to InvalidOptionTest

---
+++ src/test/java/com/twu/biblioteca/InvalidOptionTest.java
@@ -0,0 +1,21 @@
+package com.twu.biblioteca;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+public class InvalidOptionTest {
+
+    @Test
+    public void testShouldDisplayInvalidOptionMessage() {
+        Stream console = mock(Stream.class);
+        InvalidOption invalidOption = new InvalidOption(console);
+
+        invalidOption.execute(new ArrayList<>());
+
+        verify(console, times(1)).output("Please select a valid option!\n");
+    }
+}
