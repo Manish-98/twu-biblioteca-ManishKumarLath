c584b1a7055b5a586c76756305b316284e4cf24c
Manish Kumar Lath <manishlath3445@gmail.com>
2020-02-12 17:28:05 +0530

    Refactor | Migrated tests for checking out movies to CheckoutMovieTest

---
+++ src/test/java/com/twu/biblioteca/CheckoutMovieTest.java
@@ -0,0 +1,44 @@
+package com.twu.biblioteca;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class CheckoutMovieTest {
+
+    @Test
+    public void testShouldCheckoutMovieRequestedByUser() throws IOException {
+        Stream console = mock(Stream.class);
+        Movie movieOne = new Movie("Movie1", 2020, "Director1", 7.5);
+        Movie movieTwo = new Movie("Movie2", 2020, "Director2", 7.5);
+        Collection<LibraryItems> movies = new ArrayList<>(Arrays.asList(movieOne, movieTwo));
+        when(console.input()).thenReturn("Movie1");
+        CheckoutMovie checkoutMovie = new CheckoutMovie(console);
+
+        checkoutMovie.execute(movies);
+
+        assertTrue(movieOne.isCheckedOut());
+        assertFalse(movieTwo.isCheckedOut());
+    }
+
+    @Test
+    public void testShouldNotCheckoutMoviesNotInTheLibrary() throws IOException {
+        Stream console = mock(Stream.class);
+        Movie movieOne = new Movie("Movie1", 2020, "Director1", 7.5);
+        Collection<LibraryItems> movies = new ArrayList<>(Collections.singletonList(movieOne));
+        when(console.input()).thenReturn("Movie2");
+        CheckoutMovie checkoutMovie = new CheckoutMovie(console);
+
+        checkoutMovie.execute(movies);
+
+        assertFalse(movieOne.isCheckedOut());
+    }
+}
